3b8c5ce9b155a966e55c4a624a170bbcaf64f970
qqyule <qqyule@gmail.com>
2018-10-31 15:00:41 +0800

    封装axios成功

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,16 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <axios msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import axios from './components/axios.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    axios
   }
 }
 </script>
---
+++ src/utils/api.js
@@ -0,0 +1,13 @@
+/*
+ * @Author: leo
+ * @Date: 2018-10-31 14:56:59
+ * @Last Modified by: leo
+ * @Last Modified time: 2018-10-31 14:56:59
+ * axios的api路径
+ */
+export default {
+  contents: '/contents?',
+  right: '/topic/5433d5e4e737cbe96dcef312', // 正确路径
+  error: '/topc/5433d5e4e737cbe96dcef312', // url错误
+  backEnd: '/topic/5433d5e4e737cbe96dcef31' // 参数错误
+}
